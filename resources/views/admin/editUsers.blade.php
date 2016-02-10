643cc90d1193fc6aa90da895b5e8da8c0a6da5d4
Jati Pikukuh <jati.pikukuh@mail.ugm.ac.id>
2016-02-10 15:02:33 +0700

    add view-> admin, controller -> adminController, routes -> admin`

---
+++ resources/views/admin/editUsers.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Free Bootstrap Admin Template : Dream</title>
+	<!-- Bootstrap Styles-->
+    <link href="{{url('assets/css/bootstrap.css')}}" rel="stylesheet" />
+     <!-- FontAwesome Styles-->
+    <link href="{{url('assets/css/font-awesome.css')}}" rel="stylesheet" />
+        <!-- Custom Styles-->
+    <link href="{{url('assets/css/custom-styles.css')}}" rel="stylesheet" />
+     <!-- Google Fonts-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        @include('admin.navtop')
+        <!--/. NAV TOP  -->
+        @include('admin.navside')
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+			 <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="page-header">
+                            Forms Page <small>Edit data</small>
+                        </h1>
+                    </div>
+                </div> 
+                 <!-- /. ROW  -->
+              <div class="row">
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Form Edit
+                        </div>
+                        <div class="panel-body">
+                            @foreach($errors->all() as $error)
+                            <div class="alert alert-warning alert-dismissable">
+                              <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                              <strong>Whops</strong> {{ $error }}
+                            </div>
+                            @endforeach
+                            <div class="row">
+                                <div class="col-lg-8 col-lg-offset-2">
+                                @foreach($data as $apa)
+                                    <form role="form" method="post" action="{{ url('/admin/table/users/update/') }}/{{ $apa->id }}">
+                                      <div class="form-group">
+                                        <label for="nama">Nama</label>
+                                        <input type="text" class="form-control" id="nama" name="nama" value="{{ $apa->nama }}" placeholder="Nama">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="no_telp">No. Telp</label>
+                                        <input type="text" class="form-control" id="no_telp" value="{{ $apa->notelp }}" name="notelp" placeholder="No. Telp">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="Email">Email</label>
+                                        <input type="email" class="form-control" id="Email" value="{{ $apa->email }}" name="email" placeholder="Email">
+                                      </div>
+                                      <div class="form-group">
+                                        <label for="level">Status</label>
+                                        <input type="text" class="form-control" id="level" name="level" value="{{ $apa->level }}" placeholder="Level">
+                                      </div>
+                                      <input type="hidden" value="{{ csrf_token() }}" name="_token" >
+                                      <br><br>
+                                      <button type="submit" class="btn btn-default">Submit</button>
+                                    </form>
+                                @endforeach
+                                </div>
+                            </div>
+                            <!-- /.row (nested) -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+			<footer><p>All right reserved. Template by: <a href="http://webthemez.com">WebThemez</a></p></footer>
+			</div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- JS Scripts-->
+    <!-- jQuery Js -->
+    <script src="{{url('assets/js/jquery-1.10.2.js')}}"></script>
+      <!-- Bootstrap Js -->
+    <script src="{{url('assets/js/bootstrap.min.js')}}"></script>
+    <!-- Metis Menu Js -->
+    <script src="{{url('assets/js/jquery.metisMenu.js')}}"></script>
+      <!-- Custom Js -->
+    <script src="{{url('assets/js/custom-scripts.js')}}"></script>
+    
+   
+</body>
+</html>
---
+++ resources/views/admin/navside.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+
+                    <li>
+                        <a href="{{url('admin')}}"><i class="fa fa-dashboard"></i> Dashboard</a>
+                    </li>
+					<li>
+                        <a href="{{url('admin/chart')}}"><i class="fa fa-bar-chart-o"></i> Grafik</a>
+                    </li>
+                    <li>
+                        <a href="{{url('admin/table')}}"><i class="fa fa-table"></i> Tabel</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
